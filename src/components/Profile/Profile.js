18161147d49aa886939a9acca1094ccc1a9ad1f0
Eric Cavanna <almightyeric@gmail.com>
2018-02-17 11:02:05 -0800

    Make it pretty!
    - Install and integrate Semantic UI React and its accompanying CSS.
    - Display elements of GitHub profile data in a Semantic UI Card.
    - Replace Header contents with Semantic UI Menu.
    - Remove default feature list from App contents.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,21 +1,67 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import {Card, Container, Dimmer, Icon, Image, Loader, Message, Segment} from 'semantic-ui-react'
 import './profile.scss'
 
 class Profile extends Component {
-  componentDidMount () {
-    this.props.loadProfileData('gitname')
-  }
-
   render () {
     const {profileData, profileDataLoadingStatus, profileDataLoadingErrorStatus} = this.props
+
     return (
-      <div styleName='Profile'>
-        <h3>Profile</h3>
-        {profileDataLoadingStatus ? 'Loading...' : null}
-        {profileDataLoadingErrorStatus ? 'Error!' : null}
-        {!profileDataLoadingStatus && !profileDataLoadingErrorStatus && profileData.login ? profileData.login : null}
-      </div>
+      <Container styleName='Profile'>
+
+        <Dimmer.Dimmable>
+          <Dimmer inverted active={profileDataLoadingStatus || profileDataLoadingErrorStatus}>
+            <Segment basic>
+              <Message icon error={profileDataLoadingErrorStatus} hidden={!profileDataLoadingErrorStatus}>
+                <Message.Content>
+                  <Message.Header>
+                    Error
+                  </Message.Header>
+                  <p>
+                    We failed to load the profile data from GitHub.
+                  </p>
+                </Message.Content>
+              </Message>
+            </Segment>
+          </Dimmer>
+        </Dimmer.Dimmable>
+
+        {!profileDataLoadingErrorStatus &&
+          <Card>
+            <Loader indeterminate active={profileDataLoadingStatus}>Loading</Loader>
+            <Image src={profileData.avatar_url} />
+            <Card.Content>
+              <Card.Header>
+                {profileData.name}
+              </Card.Header>
+              <Card.Meta>
+                {profileData.login}
+              </Card.Meta>
+              <Card.Description>
+                {profileData.bio}
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='marker' />
+              {profileData.location}
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='user' />
+              {profileData.followers} Followers
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='fork' />
+              {profileData.public_repos} Public Repos
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='github' />
+              {profileData.public_gists} Public Gists
+            </Card.Content>
+          </Card>
+        }
+
+      </Container>
     )
   }
 }
