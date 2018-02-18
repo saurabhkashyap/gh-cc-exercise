3a2787741c46afcc90a64302417347fc5d813e33
Eric Cavanna <almightyeric@gmail.com>
2018-02-17 22:37:48 -0800

    Refactor Explorer skeleton code

--- src/components/Explorer/Explorer.js
+++ src/components/Explorer/Explorer.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Tab} from 'semantic-ui-react'
+import {Menu, Tab} from 'semantic-ui-react'
 import './explorer.scss'
 
+const panes = [
+  {
+    menuItem: (
+      <Menu.Item key='1'>
+        Overview
+      </Menu.Item>
+    ),
+    pane: (
+      <Tab.Pane>
+        <p>Content of the tab pane.</p>
+      </Tab.Pane>
+    )
+  },
+  {
+    menuItem: (
+      <Menu.Item key='2'>
+        Recent Activity
+      </Menu.Item>
+    ),
+    pane: (
+      <Tab.Pane>
+        <p>Content of the tab pane.</p>
+      </Tab.Pane>
+    )
+  }
+]
+
 const Explorer = (props) => (
-  <Tab menu={{secondary: true, pointing: true}} panes={[
-    {menuItem: 'Overview', render: () => <Tab.Pane loading attached={false}>Tab 1 Content</Tab.Pane>},
-    {menuItem: 'Repositories', render: () => <Tab.Pane loading attached={false}>Tab 2 Content</Tab.Pane>}
-  ]} />
+  <Tab
+    menu={{
+      secondary: true,
+      pointing: true
+    }}
+    panes={panes}
+  />
 )
 
 export default Explorer
