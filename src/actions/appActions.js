82ccaa969804ff54bfa0c1d305b848e317322812
Eric Cavanna <almightyeric@gmail.com>
2018-02-18 12:00:51 -0800

    Refactor multiple action creators into a common one

--- src/actions/appActions.js
+++ src/actions/appActions.js
@@ -28,35 +28,6 @@ const setProfileDataLoadingErrorStatus = (status) => {
   }
 }
 
-const fetchProfileData = (username) => {
-  return (dispatch) => {
-    dispatch(setProfileDataLoadingErrorStatus(false))
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch. HTTP status: [${response.status}] ${response.statusText}`)
-        }
-        return response.json()
-      })
-      .then((profileData) => {
-        dispatch(storeProfileData(profileData))
-        dispatch(setProfileDataLoadingStatus(false))
-      })
-      .catch((err) => {
-        console.log('Failed to either fetch or parse profile data. Error:', err.message)
-        dispatch(setProfileDataLoadingErrorStatus(true))
-        dispatch(setProfileDataLoadingStatus(false))
-      })
-  }
-}
-
-export const loadProfileData = (username) => {
-  return (dispatch) => {
-    dispatch(setProfileDataLoadingStatus(true))
-    dispatch(fetchProfileData(username))
-  }
-}
-
 const storeActivityData = (activityData) => {
   return {
     type: SET_ACTIVITY_DATA,
@@ -78,31 +49,46 @@ const setActivityDataLoadingErrorStatus = (status) => {
   }
 }
 
-const fetchActivityData = (username) => {
+export const loadProfileData = (username) => {
   return (dispatch) => {
-    dispatch(setActivityDataLoadingErrorStatus(false))
-    fetch(`https://api.github.com/users/${username}/events/public`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch. HTTP status: [${response.status}] ${response.statusText}`)
-        }
-        return response.json()
-      })
-      .then((activityData) => {
-        dispatch(storeActivityData(activityData))
-        dispatch(setActivityDataLoadingStatus(false))
-      })
-      .catch((err) => {
-        console.log('Failed to either fetch or parse activity data. Error:', err.message)
-        dispatch(setActivityDataLoadingErrorStatus(true))
-        dispatch(setActivityDataLoadingStatus(false))
-      })
+    dispatch(setProfileDataLoadingStatus(true))
+    dispatch(fetchJsonResource(`https://api.github.com/users/${username}`, {
+      dataProcessor: storeProfileData,
+      loadingStatusController: setProfileDataLoadingStatus,
+      errorStatusController: setProfileDataLoadingErrorStatus
+    }))
   }
 }
 
 export const loadActivityData = (username) => {
   return (dispatch) => {
     dispatch(setActivityDataLoadingStatus(true))
-    dispatch(fetchActivityData(username))
+    dispatch(fetchJsonResource(`https://api.github.com/users/${username}/events/public`, {
+      dataProcessor: storeActivityData,
+      loadingStatusController: setActivityDataLoadingStatus,
+      errorStatusController: setActivityDataLoadingErrorStatus
+    }))
+  }
+}
+
+const fetchJsonResource = (url, {dataProcessor, loadingStatusController, errorStatusController}) => {
+  return (dispatch) => {
+    dispatch(errorStatusController(false))
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch. HTTP status: [${response.status}] ${response.statusText}`)
+        }
+        return response.json()
+      })
+      .then((data) => {
+        dispatch(dataProcessor(data))
+        dispatch(loadingStatusController(false))
+      })
+      .catch((err) => {
+        console.log('Failed to either fetch or parse response. Error:', err.message)
+        dispatch(errorStatusController(true))
+        dispatch(loadingStatusController(false))
+      })
   }
 }
