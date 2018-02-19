353b8ea7a1cf5d0abcbc898d018a293ee43ad42c
Eric Cavanna <almightyeric@gmail.com>
2018-02-18 18:09:32 -0800

    Update copyright year to current year
    This dynamic solution automatically synchronizes the rendered year with
    the real-world year, each time the component is rendered.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './footer.scss'
 
 const Footer = props => {
+  const currentYear = (new Date()).getFullYear()
+
   return (
     <footer styleName='App-footer'>
-      <p>© 2017 <a href='https://clearcapital.com'>ClearCapital.com</a>, Inc. All Rights Reserved.</p>
-
+      <p>&copy; {currentYear} <a href='https://clearcapital.com'>ClearCapital.com</a>, Inc. All Rights Reserved.</p>
     </footer>
   )
 }
